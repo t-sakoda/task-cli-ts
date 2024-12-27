c92cdcf9b2002f27d5931e76accf68069a7f21b0
t-sakoda <sakoda.taro.229@gmail.com>
2024-12-27 09:11:13 +0900

    🎨 Biomeの設定ファイルを追加し、依存関係を更新。コードスタイルを統一。

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2);
-console.log("Arguments:", args);
+const args = process.argv.slice(2)
+console.log('Arguments:', args)
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest';
+import {describe, expect, it} from 'vitest'
 
 describe('index', () => {
   it('should return true', () => {
-    expect(true).toBe(true);
-  });
+    expect(true).toBe(true)
+  })
 })
