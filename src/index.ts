400b1eef3bb9225b39817924b49f0fcf077ab1a6
t-sakoda <sakoda.taro.229@gmail.com>
2024-12-28 12:40:31 +0900

    ✨ タスク管理機能をTaskControllerクラスに移行し、引数の検証を追加

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,18 @@
 #!/usr/bin/env node
 
+import {TaskController} from './presentation/taskController'
+
 const args = process.argv.slice(2)
 
-// Validate arguments
-if (args.length === 0) {
-  console.error('No arguments provided')
+const taskController = new TaskController()
+try {
+  taskController.run(args)
+  process.exit(0)
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.error(error.message)
+  } else {
+    console.error('Unexpected error occurred')
+  }
   process.exit(1)
 }
-
-switch(args[0]) {
-  case 'add':
-    console.log('Adding...')
-    break
-  case 'update':
-    console.log('Updating...')
-    break
-  case 'delete':
-    console.log('Deleting...')
-    break
-  case 'mark-in-progress':
-    console.log('Marking as in progress...')
-    break
-  case 'mark-done':
-    console.log('Marking done...')
-    break
-  case 'list': {
-    switch (args[1]) {
-      case undefined:
-        console.log('Listing all tasks...')
-        break
-      case 'done':
-        console.log('Listing done tasks...')
-        break
-      case 'todo':
-        console.log('Listing todo tasks...')
-        break
-      case 'in-progress':
-        console.log('Listing tasks in progress...')
-        break
-      default:
-        console.error('Invalid list option')
-        process.exit(1)
-    }
-    break
-  }
-  default:
-    console.error('Invalid command')
-    process.exit(1)
-}
-
-process.exit(0)
