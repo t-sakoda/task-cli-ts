357a4b1c61823a2eb8baa0cfa96248b172885beb
t-sakoda <sakoda.taro.229@gmail.com>
2024-12-29 21:43:40 +0900

    ✨ タスクを完了としてマークする機能を実装し、エラーハンドリングを追加。関連するユニットテストを作成。

--- src/useCase/markTaskDoneUseCase.ts
+++ src/useCase/markTaskDoneUseCase.ts
@@ -1,9 +1,10 @@
+import {type Task, TaskStatus} from '../domain/task'
 import type {ITaskRepository} from '../domain/taskRepository'
 
 export const MarkTaskDoneUseCaseErrorCode = {
-  ID_REQUIRED: 'ID_REQUIRED',
-  TASK_NOT_FOUND: 'TASK_NOT_FOUND',
-  INTERNAL_ERROR: 'INTERNAL_ERROR',
+  ID_REQUIRED: 'IdRequired',
+  TASK_NOT_FOUND: 'TaskNotFound',
+  INTERNAL_ERROR: 'InternalError',
 } as const
 export type MarkTaskDoneUseCaseErrorCode =
   (typeof MarkTaskDoneUseCaseErrorCode)[keyof typeof MarkTaskDoneUseCaseErrorCode]
@@ -20,6 +21,23 @@ export class MarkTaskDoneUseCase {
   }
 
   run(id: string) {
-    throw new Error('Not implemented')
+    if (!id) {
+      throw new Error(MarkTaskDoneUseCaseErrorCode.ID_REQUIRED)
+    }
+    let task: Task | undefined
+    try {
+      task = this.taskRepository.find(id)
+    } catch {
+      throw new Error(MarkTaskDoneUseCaseErrorCode.INTERNAL_ERROR)
+    }
+    if (!task) {
+      throw new Error(MarkTaskDoneUseCaseErrorCode.TASK_NOT_FOUND)
+    }
+    task.mark(TaskStatus.DONE)
+    try {
+      this.taskRepository.update(task)
+    } catch {
+      throw new Error(MarkTaskDoneUseCaseErrorCode.INTERNAL_ERROR)
+    }
   }
 }
