dd8a258aba56d02802e44483639e14562ebd6992
t-sakoda <sakoda.taro.229@gmail.com>
2024-12-29 10:14:38 +0900

    ✨ AddTaskUseCaseのユニットテストを改善し、モックリポジトリを使用するように変更

--- test/useCase/addTaskUseCase.test.ts
+++ test/useCase/addTaskUseCase.test.ts
@@ -1,13 +1,9 @@
 import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest'
 import {TaskStatus} from '../../src/domain/task'
-import {TaskRepository} from '../../src/infra/taskRepository'
 import {AddTaskUseCase} from '../../src/useCase/addTaskUseCase'
+import {mockTaskRepository} from '../shared/mockTaskRepository'
 
-vi.mock(import('../../src/infra/taskRepository'), () => {
-  const TaskRepository = vi.fn()
-  TaskRepository.prototype.insert = vi.fn()
-  return {TaskRepository}
-})
+const taskRepository = mockTaskRepository()
 
 describe('AddTaskUseCase', () => {
   const mockDate = new Date('2025-01-01 09:00:00')
@@ -20,11 +16,10 @@ describe('AddTaskUseCase', () => {
   })
   describe('Given a correct argument', () => {
     it('calls TaskRepository.add with the correct task name', () => {
-      const taskRepository = new TaskRepository()
       const useCase = new AddTaskUseCase({taskRepository})
       const description = 'Do the laundry'
       useCase.run(description)
-      expect(TaskRepository.prototype.insert).toHaveBeenCalledWith({
+      expect(taskRepository.insert).toHaveBeenCalledWith({
         id: expect.any(String),
         description,
         status: TaskStatus.TODO,
@@ -36,7 +31,6 @@ describe('AddTaskUseCase', () => {
 
   describe('Given an empty argument', () => {
     it('throws an error', () => {
-      const taskRepository = new TaskRepository()
       const useCase = new AddTaskUseCase({taskRepository})
       expect(() => useCase.run('')).toThrow('No task name provided')
     })
